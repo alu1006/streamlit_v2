8a88af15995a28d127e4c2324e2263104da75946
alu1006 <andy8050802@gmail.com>
2024-10-30 05:14:12 +0800

    登入

--- db.py
+++ db.py
@@ -1,23 +1,32 @@
 import sqlite3
+import sqlitecloud
+import dotenv
+import os
+dotenv.load_dotenv()
+def conn_db(db_name):
+    conn = sqlitecloud.connect(f"sqlitecloud://cxecbykinz.sqlite.cloud:8860/{db_name}?apikey={os.getenv('SQL_KEY')}")
+    return conn
 
 # 建立使用者資料表
 def create_usertable():
-    conn = sqlite3.connect('users.db')
+    conn = conn_db('users')
+    
     c = conn.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, password TEXT)')
     conn.commit()
     conn.close()
+
 # 新增使用者
 def add_user(username, password):
-    conn = sqlite3.connect('users.db')
+    conn = conn_db('users')
     c = conn.cursor()
     c.execute('INSERT INTO users(username, password) VALUES (?, ?)', (username, password))
     conn.commit()
     conn.close()
-    
+# add_user('admin', 'admin') 
 # 驗證使用者
 def login_user(username, password):
-    conn = sqlite3.connect('users.db')
+    conn = conn_db('users')
     c = conn.cursor()
     c.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
     data = c.fetchone()
--- sqlitecloud_test.py
+++ sqlitecloud_test.py
@@ -1,18 +1,19 @@
 import sqlitecloud
 import db
+import dotenv
+import os
+dotenv.load_dotenv()
 
-conn = sqlitecloud.connect("sqlitecloud://cxecbykinz.sqlite.cloud:8860/chinook.sqlite?apikey=1Yf0LpKlXL1VG5K7ky4aeHNDfFeOCqs0WIMP79XSnSo")
+conn = sqlitecloud.connect(f"sqlitecloud://cxecbykinz.sqlite.cloud:8860/users?apikey={os.getenv('SQL_KEY')}")
 
 # You can autoselect the database during the connect call
 # by adding the database name as path of the SQLite Cloud
 # connection string, eg:
 # conn = sqlitecloud.connect("sqlitecloud://myhost.sqlite.cloud:8860/mydatabase?apikey=myapikey")
-db_name = "users"
-conn.execute(f"USE DATABASE {db_name}")
+# db_name = "chinook.sqlite"
+# conn.execute(f"USE DATABASE {db_name}")
 
-cursor = conn.execute('CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, password TEXT)')
-result = cursor.fetchone()
-
-print(result)
-
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, password TEXT)')
+conn.commit()
 conn.close()
